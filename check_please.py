d1d179e5bed23dc1666311b07cc54d172e58ac8e
Matthew Lawrence <matt05law@gmail.com>
2018-09-01 17:08:44 -0400

    Added check splitting app

---
+++ check_please.py
@@ -0,0 +1,19 @@
+# Creating a check splitting app
+import math
+
+# adding if/raise error
+def split_check(total, number_of_people):
+    if number_of_people <= 1:
+        raise ValueError("More than one person is required to split the check")
+    return math.ceil(total / number_of_people)
+
+# Testing out exceptions   
+try:
+    total_due = float(input("What is the total? "))
+    number_of_people = int(input("How many people? "))
+    amount_due = split_check(total_due, number_of_people)
+except ValueError as err:
+    print("Oh no! That's not a valid value. Try again...")
+    print("({})".format(err))
+else:
+    print("Each person owes ${}".format(amount_due))
