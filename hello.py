475ff7fc1a0235cd630b79674b818147e456285d
Matthew Lawrence <matt05law@gmail.com>
2018-09-01 16:20:16 -0400

    Updated notes and added file to test out functions

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+# Testing out asking for input and having different responses based on that input
+
 first_name = input("What is your first name?  ")
 print("Hello, ", first_name)
 if first_name == "Matt":
@@ -5,7 +7,7 @@ if first_name == "Matt":
 elif first_name == "Aaron":
     print(first_name, "is learning with fellow students in the Community! Me too!")
 else:
-    # This is juat in case we have a younger user wwho can't read yet
+    # This is just in case we have a younger user wwho can't read yet
     age = int(input("How old are you? "))
     if age <= 6:
         print("Wow you're {}!   If you're confident with your reading already...".format(age)) 
