751f688904118a8cea9e23f97ae44b0b885f11e1
Matthew Lawrence <matt05law@gmail.com>
2018-09-01 17:29:13 -0400

    Added while loop

--- password_checker.py
+++ password_checker.py
@@ -1,2 +1,7 @@
 # Password checking app
 password = input("Please enter the super secret password: ")
+attempt_count = 1
+while password != 'opensesame':
+    password = input("Invalid password, try again: ")
+    attempt_count += 1
+print("Welcome to secret town")
