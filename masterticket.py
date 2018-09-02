dbd8ada6f674861058469072ba9896b70c61d4d5
Matthew Lawrence <matt05law@gmail.com>
2018-09-01 21:47:12 -0400

    Finalized ticket requests

--- masterticket.py
+++ masterticket.py
@@ -1,20 +1,26 @@
 TICKET_PRICE = 10
 
-tickets_remaining = 100  
+tickets_remaining = 100
 
-# Output how many tickets are remaining using the tickets_remaining variable
 
-print("There are {} tickets remaining.".format(tickets_remaining))
-
-# Gather the user's name and assign it to a new variable
-name = input("What is your name?  ")
-
-# Prompt the user by name and ask how many tickets they would like
-num_tickets = input("Hello {}! How many tickets would you like?  ".format(name))
-num_tickets = int(num_tickets)
-
-# Calculcate the price (number of tickets multiplied by the price) and assign it to a variable
-amount_due = TICKET_PRICE * num_tickets
-
-# Output the price to the screen
-print("{}, that will cost ${}.  ".format(name, amount_due))
+while tickets_remaining >= 1:
+    print("There are {} tickets remaining.".format(tickets_remaining))
+    name = input("What is your name?  ")
+    num_tickets = input("Hello {}! How many tickets would you like?  ".format(name))
+    try:
+        num_tickets = int(num_tickets)
+        if num_tickets > tickets_remaining:
+            raise ValueError("There are only {} tickets remaining".format(tickets_remaining))
+    except ValueError as err:
+        print("Oh no! That's not a valid value. {}. Try again...".format(err))
+    else:
+        amount_due = TICKET_PRICE * num_tickets
+        print("{}, that will cost ${}".format(name, amount_due))
+        should_proceed = input("Do you want to proceed?  Y/N  ")
+        if should_proceed.lower() == "y":
+            # TODO: Gather credit card information and process it.
+            print("SOLD!")
+            tickets_remaining -= num_tickets
+        else:
+            print("Thank you anyways, {}!".format(name))
+print("Sorry the tickets are all sold out!!! :(")
