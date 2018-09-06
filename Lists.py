96a348211fafda1c95cee5718d27c3751385f0c0
Matthew Lawrence <matt05law@gmail.com>
2018-09-06 18:28:52 -0400

    Testing out different values and combing lists

--- Lists.py
+++ Lists.py
@@ -5,3 +5,24 @@ bool(languages)
 banner = list("Congratulations")
 print(banner)
 
+temperatures = []
+temperatures.append(98.6)
+temperatures.append(99.4)
+temperatures
+er_temps = [102.2, 103.5, 101.1, 99.9]
+temperatures.extend(er_temps)
+primary_care_doctors = ["Dr. Scholls", "Dr. Pepper"]
+er_doctors = ["Doug", "Susan"]
+all_doctors = primary_care_doctors + er_doctors
+all_doctors
+primary_care_doctors
+temperatures.append(99)
+temperatures.append("Burning up")
+
+
+attendees = ["Ken", "Alena", "Treasure"]
+attendees.append("Ashley")
+attendees.extend(["James", "Guil"])
+optional_invitees = ["Ben J.", "Dave"]
+potential_attendees = attendees + optional_invitees
+print("There are", len(potential_attendees), "potential attendees currently")
