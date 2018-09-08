438f7419c92084fbbeb84b8b0d182692629e5679
Matthew Lawrence <matt05law@gmail.com>
2018-09-08 08:19:39 -0400

    forin loops

--- Lists.py
+++ Lists.py
@@ -19,6 +19,17 @@ primary_care_doctors
 temperatures.append(99)
 temperatures.append("Burning up")
 
+books = [
+    "Learning Python: Powerful Object-Oriented Programming - Mark Lutz",
+    "Automate the Boring Stuff with Python: Practical Programming for Total Beginners - Al Sweigart",
+    "Python for Data Analysis - Wes McKinney",
+    "Fluent Python: Clear, Concise, and Effective Programming - Luciano Ramalho",
+    "Python for Kids: A Playful Introduction To Programming - Jason R. Briggs",
+    "Hello Web App: Learn How to Build a Web App - Tracy Osborn",
+]
+
+print("Suggested gift: {}".format(books[0]))
+
 
 attendees = ["Ken", "Alena", "Treasure"]
 attendees.append("Ashley")
@@ -26,3 +37,10 @@ attendees.extend(["James", "Guil"])
 optional_invitees = ["Ben J.", "Dave"]
 potential_attendees = attendees + optional_invitees
 print("There are", len(potential_attendees), "potential attendees currently")
+
+for attendee in attendees:
+    print(attendee)
+
+print("Books:")
+for book in books:
+    print("* " + book)
